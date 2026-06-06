cb93a5cf4a671bf56580be32d8c18649fa750bbe
jessikabunga <149929654+jessikabunga@users.noreply.github.com>
2026-06-07 02:27:05 +0700

    fixed database

--- database.py
+++ database.py
@@ -1,16 +1,15 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "postgresql://postgres:database123@localhost:5432/capstone_db")
+DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+if not DATABASE_URL:
+    DATABASE_URL = "postgresql://postgres:database123@localhost:5432/capstone_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
