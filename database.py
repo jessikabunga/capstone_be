013dda5ea75601ab722cd06f79698e44ac2fb09f
Nur Satria <satriakusumah082@gmail.com>
2026-06-06 14:28:51 +0700

    ganti url

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:Kusumah.ub1@localhost/capstone_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:database123@localhost/capstone_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
