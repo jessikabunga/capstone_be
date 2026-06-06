335a8095ccd4ce6433c7f1772b6dd3e34ca477d9
jessikabunga <149929654+jessikabunga@users.noreply.github.com>
2026-06-07 02:09:48 +0700

    fixed database

--- database.py
+++ database.py
@@ -1,11 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:database123@localhost/capstone_db"
+DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "postgresql://postgres:database123@localhost/capstone_db")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
